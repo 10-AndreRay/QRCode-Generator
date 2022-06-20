5adf3dad371f436361114dfa397563f2e8b3fa2f
10-AndreRay <andreray053@gmail.com>
2022-06-20 13:14:56 -0300

    upload files

---
+++ js/main.js
@@ -0,0 +1,36 @@
+const btnGenerateQRCode = document.querySelector("#btn-generate-qr");
+const inputColor = document.querySelector("#color-bg-qr");
+const inputText = document.querySelector("#content");
+const imgQrCode = document.querySelector("img");
+
+btnGenerateQRCode.addEventListener("click", generateQRCode);
+
+async function generateQRCode() {
+    const color = getColor();
+    const content = getContent();
+
+    const urlQrCode = await getQrCode(content, color);
+
+    setImg(urlQrCode);
+};
+
+function getColor() {
+    return inputColor.value.split("#")[1];
+};
+
+function getContent() {
+    return inputText.value;
+};
+
+async function getQrCode(content, color) {
+    const urlBase = "https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=";
+    const url = urlBase + `${content}&color=${color}`;
+
+    const response = await fetch(url);
+    
+    return response.url;
+};
+
+function setImg(url) {
+    imgQrCode.setAttribute("src", url);
+};
